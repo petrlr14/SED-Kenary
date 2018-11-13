1b1ee3f55a23171259801591355fe48819a3715c
petrlr14 <pedrodeveloper14@gmail.com>
2018-11-13 01:41:25 +0000

    Login
    login template done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* const app={
+    init(){
+        let button=document.querySelector("#form-button");
+        button.addEventListener("click", event=>{
+            event.preventDefault();
+        });
+    }
+}; */
+
+/* window.addEventListener("load", app.init()); */

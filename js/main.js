96bb29c10b8421e2dd6b04c7291f2f9dc58e2af5
petrlr14 <pedrodeveloper14@gmail.com>
2018-11-14 03:40:58 +0000

    avance

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 'use strict';
 
-/* const app={
+ const app={
     init(){
         let button=document.querySelector("#form-button");
         button.addEventListener("click", event=>{
             event.preventDefault();
+            let form=document.forms["login"];
+            console.log(form.elements["username"].value);
+            console.log(form.elements["password"].value);
         });
     }
-}; */
+};
 
-/* window.addEventListener("load", app.init()); */
+window.addEventListener("load", app.init());
